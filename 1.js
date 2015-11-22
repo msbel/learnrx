953c8244e3ed8cd627576f825a95b303e53f3214
msbel <msb89@yandex.ru>
2015-11-22 19:22:38 +0300

    Exercise 1 completed

---
+++ 1.js
@@ -0,0 +1,9 @@
+function f(console) {
+    var names = ["Ben", "Jafar", "Matt", "Priya", "Brian"],
+        counter;
+
+    for (counter = 0; counter < names.length; counter++) {
+        console.log(names[counter]);
+    }
+}
+f(console)
